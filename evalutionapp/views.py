69635232dd031c7a741a7fbdb927ca9331c9caf5
thanujakmt <thanujakmsdr@gmail.com>
2024-08-19 15:02:46 +0530

    frontend done

--- evalutionapp/views.py
+++ evalutionapp/views.py
@@ -11,3 +11,12 @@ def testview(request):
 
 def user_face_view(request):
     return render(request,'components/user_face.html')
+
+def app_view(request):
+    return render(request,'components/app.html')
+
+def login_view(request):
+    return render(request,'components/login.html')
+
+def register_view(request):
+    return render(request,'components/register.html')
