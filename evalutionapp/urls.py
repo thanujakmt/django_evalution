bae167ed72e6411f54dcac7fb78ff7af55dd3dad
thanujakmt <thanujakmsdr@gmail.com>
2024-08-19 16:35:17 +0530

    doing login authentication

--- evalutionapp/urls.py
+++ evalutionapp/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('',views.testview,name='homepage'),
-    path('user_face', views.user_face_view ,name='userface'),
-    path('app',views.app_view,name='app'),
-    path('login',views.login_view,name='login'),
-    path('register',views.register_view,name='register'),
+    path('user_face/', views.user_face_view ,name='userface'),
+    path('app/',views.app_view,name='app'),
+    path('login/',views.login_view,name='login'),
+    path('register/',views.register_view,name='register'),
 ]
--- evalutionapp/views.py
+++ evalutionapp/views.py
@@ -1,22 +1,61 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from . import models
 
 # Create your views here.
-
+@login_required(login_url='/login/')
 def testview(request):
     categories = models.Category.objects.all()
     subcategories = models.SubCategory.objects.all()
     return render(request,'homepage/homepage.html',{"categories":categories,"subcategories":subcategories})
 
+@login_required(login_url='/login/')
 def user_face_view(request):
     return render(request,'components/user_face.html')
 
+@login_required(login_url='/login/')
 def app_view(request):
     return render(request,'components/app.html')
 
+def logout_view(request):
+    
+    if request.method == 'POST':
+        logout(request)
+        return redirect('login')
+
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request,username = username, password = password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('homepage')
+
     return render(request,'components/login.html')
 
 def register_view(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        if password != confirm_password:
+            return redirect('register')
+        
+        user = User.objects.create(username = username, email= email)
+        user.set_password(password)
+        user.save()
+        print(user)
+        return redirect('login')
+
     return render(request,'components/register.html')
